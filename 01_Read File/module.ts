87e8f90b1f33b80868b58d8d2a9bf13030996d97
Deep Detroja <deepdetroja1223@gmail.com>
2026-06-21 09:38:11 +0530

    bun init

---
+++ 01_Read File/module.ts
@@ -0,0 +1,3 @@
+export  const makeName = (firstName:string,lastName:string)=>{
+    return firstName + " " + lastName;
+};
---
+++ index.ts
@@ -0,0 +1,7 @@
+import  info  from "./01_Read File/info.txt";
+import user from "./01_Read File/user.json"
+import { makeName } from "./01_Read File/module";
+
+console.log(info);
+console.log(user);
+console.log(makeName("Deep","Detroja"));
