beb359b3b17038a6dac43cbd1bc5cc3b20378795
Deep Detroja <deepdetroja1223@gmail.com>
2026-06-21 09:47:56 +0530

    Environment Variables

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
-import  info  from "./01_Read File/info.txt";
-import user from "./01_Read File/user.json"
-import { makeName } from "./01_Read File/module";
+// 01- Read files 
 
-console.log(info);
-console.log(user);
-console.log(makeName("Deep","Detroja"));
+// import  info  from "./01_Read File/info.txt";
+// import user from "./01_Read File/user.json"
+// import { makeName } from "./01_Read File/module";
+
+// console.log(info);
+// console.log(user);
+// console.log(makeName("Deep","Detroja"));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 02- Bun Environment => "env file", "env.ts file",
+
+// way-1
+// const PORT=process.env.PORT
+// console.log(PORT)
+
+// way-2
+// const PORT=Bun.env.PORT
+// console.log(PORT)
+
+// way-3
+// const PORT=import.meta.env.PORT
+// console.log(PORT)
+
+
+
